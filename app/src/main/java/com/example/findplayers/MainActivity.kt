d3fbd2c3e790845696d0db226f6bf5b46004390f
Dias <dias.dzhakupov.68@mail.ru>
2021-02-09 11:49:52 +0600

    fix warnings and add my first Intent

--- app/src/main/java/com/example/findplayers/MainActivity.kt
+++ app/src/main/java/com/example/findplayers/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.findplayers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val main_btn=findViewById<View>(R.id.main_btn)
+        main_btn.setOnClickListener {
+            val intent= Intent(this, activity_ranked::class.java)
+            startActivity(intent)
+        }
     }
 }
